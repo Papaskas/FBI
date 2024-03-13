d3d4dc6b0a0dc1ea4bcf1a34a7c9e5e1b9a50a35
papaskas <pavel.glugxix@gmail.com>
2024-03-13 18:43:24 +0300

    Added several new icons and desktop.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DesktopComponent } from '@app/core/desktop/desktop.component';
+import { AppComponent } from '@app/app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'desktop', component: DesktopComponent },
+];
---
+++ src/app/core/desktop/desktop.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-desktop',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './desktop.component.html',
+  styleUrl: './desktop.component.css',
+})
+export class DesktopComponent {
+  title = 'FBI';
+}
