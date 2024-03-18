f634537a8d25dc4bfcfce934514e0600d19a63f6
papaskas <pavel.glugxix@gmail.com>
2024-03-18 12:49:42 +0300

    Added global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
 import { DesktopComponent } from '@components/desktop/desktop.component';
 
-export const routes: Routes = [{ path: 'desktop', component: DesktopComponent }];
+export const routes: Routes = [{ path: '', component: DesktopComponent }];
