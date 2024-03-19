f8bcf0d92f8da130f4de4355a7b3a1706b13d88f
papaskas <pavel.glugxix@gmail.com>
2024-03-19 14:11:43 +0300

    refactor path desktop shortcut component

--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DockBarComponent } from '@components/dock-bar/dock-bar.component';
-import { DesktopShortcutComponent } from '@components/desktop-shortcut/desktop-shortcut.component';
+import { DesktopShortcutComponent } from '@components/desktop/desktop-shortcut/desktop-shortcut.component';
 import { ApplicationComponent } from '@components/application/application.component';
 
 @Component({
